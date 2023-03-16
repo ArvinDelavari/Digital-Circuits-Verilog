9269b8a31c45ec668f13277ec23467ddda0d30ab
Arvin Delavari <86099054+ArvinDelavari@users.noreply.github.com>
2023-03-16 09:58:02 +0330

    Add files via upload

---
+++ Full_adder/Full_Adder.v
@@ -0,0 +1,8 @@
+module fulladder (input [3:0] a,  
+                  input [3:0] b,  
+                  input c_in,  
+                  output c_out,  
+                  output [3:0] sum);  
+  
+   assign {c_out, sum} = a + b + c_in;  
+endmodule  
